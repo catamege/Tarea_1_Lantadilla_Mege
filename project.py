6c69d09cf37161ddaf308d74f7a52c9179501a51
catamege <catamegecastro@gmail.com>
2025-04-02 23:29:41 -0300

    fix:finish instructions about how git works

--- project.py
+++ project.py
@@ -1,1 +1,23 @@
-print("hello world")
+print("paso cero: poner git pull en el terminal para tener última version")
+print(
+    "paso uno: programar lo que quieres y cuando termines poner git add . "
+    "en el terminal"
+)
+print(
+    "paso dos: hacer un commit con el comentario del cambio, puede ser wip "
+    "(work in progress), feat (nueva caracteristica que hice), fix (arregle "
+    'algo de la otra. poner git commit -m"feat:poto en el terminar"'
+)
+print("paso tres: publicar ultima version con git push")
+
+print(
+    "para ver los cambios que he hecho abro la pestaña de changes. "
+    "Se abre el working tree donde aparecen los cambios respecto a la última "
+    "versión de la web, los verdes son cambios nuevos y rojos son cosas que "
+    "se eliminaron"
+)
+
+print(
+    "guardar para guardar cambios en el computador y no en la web, es decir "
+    "no es un pull ni un push"
+)
